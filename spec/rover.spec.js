9569abababda1a3d3d61b5fe27d09a8a8f4bae8e
Jenn <61742942+hashbrowns94@users.noreply.github.com>
2021-07-06 15:16:39 +0000

    complete through test 9

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,29 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  // it ('constructor sets position and default values for mode and generatorWatts', function() {
+  //   let rover = new Rover(15486);
+  //   expect(rover.position).toEqual(15486);
+  //   expect(rover.mode).toEqual('NORMAL');
+  //   expect(rover.generatorWatts).toEqual(110);
+  // });
+
+  it('response returned by receiveMessage contains name of message', function() {
+    let rover = new Rover(15486);
+    let message = new Message('New message!');
+    let output = rover.receiveMessage(message);
+    expect(output.name).toEqual(message.name);
+  });
+
+  it('response returned by receiveMessage includes two results if two commands are sent in the message', function() {
+    let rover = new Rover(15486);
+    let message = new Message('New message!', ['MOVE', 'MODE_CHANGE']);
+    let output = rover.receiveMessage(message);
+    expect(output.results.length === 2).toBeTrue();
+  });
+
+  it('responds correctly to status check command', function() {
+    
+  })
 
 });
