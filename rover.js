b4178fcd8927055684cc2b41e2d813d9bb70a519
Jenn <61742942+hashbrowns94@users.noreply.github.com>
2021-07-15 19:50:57 +0000

    final commit

--- rover.js
+++ rover.js
@@ -1,7 +1,6 @@
 const Message = require('./message.js');
 const Command = require('./command.js');
 
-
 class Rover {
    constructor(position) {
      this.position = position;
@@ -9,19 +8,58 @@ class Rover {
      this.generatorWatts = 110;
    }
    receiveMessage(message){
-     let msg = {
-       name: message.name,
-       results: message.commands
-     }
-     return msg;
-   }
+       let msg = {
+       message: message.name,
+       results: []
+    }
+    if (message.commands) {
+      for (let i = 0; i < message.commands.length; i++) {
+        let cmdType = message.commands[i].commandType;
+        let cmdVal = message.commands[i].value;
+        if (cmdVal && typeof cmdVal != 'number') {
+          this.mode = cmdVal;
+        }
+        if (cmdType === 'STATUS_CHECK') {
+          let status = {
+            completed: true,
+            roverStatus: {
+              mode: this.mode, 
+              generatorWatts: this.generatorWatts, 
+              position: this.position
+              }
+            }
+          msg.results.push(status);
+        }
+        if (cmdType === 'MODE_CHANGE') {
+          let modeStatus = {
+            completed: true
+          }
+          msg.results.push(modeStatus);
+        }
+        if (cmdType === 'MOVE') {
+          if (this.mode === 'LOW_POWER') {
+            let moveStatus = {
+              completed: false
+            }
+              msg.results.push(moveStatus);
+          } else if (typeof cmdVal === 'number' && this.mode === 'NORMAL') {
+              let moveStatus = {
+              completed: true
+              }
+              this.position = cmdVal
+              msg.results.push(moveStatus);
+          } else if (this.mode === 'NORMAL') {
+              let moveStatus = {
+              completed: true
+              }
+            msg.results.push(moveStatus);
+          }  
+        }
+      }
+      return msg;
+    }
+    return msg;
+  } 
 }
-
-let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-let message = new Message('Test message with two commands', ['MODE_CHANGE', 'LOW_POWER']);
-let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-let response = rover.receiveMessage(message);
-
-console.log(response);
 
 module.exports = Rover;
